680f7676511d7df98c0efec37b2b2b542f990bcf
jdk9209 <136757302+jdk9209@users.noreply.github.com>
2023-06-27 00:37:38 +0700

    Improve algorithm efficiency

---
+++ jungle_waterfall_xylophone.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Hands on Healing")
+
+	//Day 1
+	// Step 1: Deep Relaxation
+	fmt.Println("Step 1: Deep Relaxation")
+	fmt.Println("To begin, find a comfortable seated position, closing your eyes and breathing deeply. Take a few moments to aim for true relaxation before beginning.")
+
+	// Step 2: Mindful Awareness
+	fmt.Println("Step 2: Mindful Awareness")
+	fmt.Println("Now, focus on your inner landscape. Take a few minutes to observe and identify any physical sensations, emotions, or thoughts that you may be experiencing.")
+
+	// Step 3: Mental Centering
+	fmt.Println("Step 3: Mental Centering")
+	fmt.Println("Feeling the peace that comes with observing your feelings without judgement, allow your mind to center itself in the present moment. Take a few minutes to observe the sensations of your body and the thoughts of your mind.")
+	
+	// Step 4: Self-Reflection
+	fmt.Println("Step 4: Self-Reflection")
+	fmt.Println("Now, take some time to reflect on how you feel physically, emotionally, and mentally. Take a few moments to observe how you are feeling and any areas that are in need of healing.")
+
+	// Step 5: Connecting with the Body
+	fmt.Println("Step 5: Connecting with the Body")
+	fmt.Println("Now, slowly bring your awareness to your physical body. Observe any areas of tension or discomfort, and use your breath to guide you into a relaxed state of being. Take some time to explore any areas of the body that are seeking attention.")
+
+	// Step 6: Connecting with the Mind
+	fmt.Println("Step 6: Connecting with the Mind")
+	fmt.Println("Now, bring your awareness to your mental state. Observe any thoughts, emotions, or feelings that may be present. Take some time to explore any areas of the mind that are seeking attention or healing.")
+
+	// Step 7: Connecting with the Spirit
+	fmt.Println("Step 7: Connecting with the Spirit")
+	fmt.Println("Now, bring your awareness to your spiritual self. Observe any parts of your being that feel disconnected or out of alignment. Take some time to explore the spiritual aspects of your being that are seeking attention or healing.")
+
+	// Step 8: Visualization
+	fmt.Println("Step 8: Visualization")
+	fmt.Println("Now, take a few moments to visualize the healing that your body, mind, and spirit need. Imagine the energy of healing flowing through each and every cell of your being. Take a few moments to feel the energy of healing.")
+
+	// Step 9: Compassionate Releasing
+	fmt.Println("Step 9: Compassionate Releasing")
+	fmt.Println("Now, feel the energy of healing and loving-kindness flowing out from your body, mind, and spirit. Visualize any physical or emotional discomfort, pain, or anger being released and filled with healing energy. Take a few moments to feel this deep sense of well-being.")
+
+	// Step 10: Gratitude and Blessing
+	fmt.Println("Step 10: Gratitude and Blessing")
+	fmt.Println("Now, take a few moments to feel gratitude and thankfulness for the healing energy that you have received. Visualize yourself being surrounded by a bubble of light and love, and sense the healing energy expanding and enveloping your entire being.")
+
+	// Step 11: Integration
+	fmt.Println("Step 11: Integration")
+	fmt.Println("Finally, take a few moments to integrate the healing that you have received. Imagine how you will feel and behave once you have fully integrated the new energy into your body, mind, and spirit. Take a few minutes to observe the ways in which your life will be transformed by the healing energy.")
+
+	// Step 12: Grounding and Balance
+	fmt.Println("Step 12: Grounding and Balance")
+	fmt.Println("When you are ready, slowly open your eyes and return to the physical world. Take some time to ground yourself and balance your energy. Feel the peace and joy that comes with being in harmony with yourself and the world around you.")
+
+	fmt.Println("Congratulations! You have completed Day 1 of Hands on Healing.")
+}
